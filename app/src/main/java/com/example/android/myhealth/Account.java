3fdc2eae70f7b9c81a3844a3b9c4238153faf827
Nicole <nicole.muswanya@strathmore.edu>
2019-06-03 13:55:01 +0300

    Navigation drawer implementated

---
+++ app/src/main/java/com/example/android/myhealth/Account.java
@@ -0,0 +1,20 @@
+package com.example.android.myhealth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Account extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.account, container,false);
+        return myView;
+    }
+}
